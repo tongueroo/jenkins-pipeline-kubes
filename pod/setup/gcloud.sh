bfd81e94386e3ab0be5148f092a9f73b4f218f6d
Tung Nguyen <tongueroo@gmail.com>
2022-11-04 12:07:18 +0000

    update setup gcloud.sh

--- pod/setup/gcloud.sh
+++ pod/setup/gcloud.sh
@@ -2,3 +2,4 @@
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-408.0.1-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-408.0.1-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh
+ln -s /google-cloud-sdk/bin/gcloud /usr/local/bin/gcloud
